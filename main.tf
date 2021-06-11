7698ed9baf2437e017c07184bfe4b2de5929c1df
msherid2 <msherid2@gmail.com>
2021-06-11 10:07:12 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
     organization = "infrastructure-pipelines-workshop"
 
     workspaces {
-      name = "john-d-consul"
+      name = "matthew-s-consul"
     }
   }
   required_providers {
